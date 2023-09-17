ae3bc76bddf2c90bc1ec7351b5b551eb155c8230
Aboood <abdu3793@stud.kea.dk>
2023-09-17 21:31:03 +0200

    that was it ..

---
+++ js/api-keys.js
@@ -0,0 +1,5 @@
+﻿// API KEYS
+const mapboxApiKey = "pk.eyJ1IjoiYWJvb29kc2EiLCJhIjoiY2xtYXcwcDZtMHp3ODNjcXE0YWY4dmNrMyJ9.0sDQp8tgynWP70CQOLZkrw";
+const eventsApiKey = "dGZ6cx8vkDKVJptA5d5stXMzGP7GeObZ";
+const weatherApiKey = "c233524953b26f5885f25e5f96816ad9";
+export { mapboxApiKey, eventsApiKey, weatherApiKey };
